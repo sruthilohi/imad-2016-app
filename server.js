e10864709c302670c7f365e0bc5fe57e0af4a74b
sruthilohi <sruthilohi@gmail.com>
2016-09-24 16:26:42 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/article-two', function(req,res){
+ req.send('article two will be served here');   
+});
+app.get('/article-three', function(req,res){
+ req.send('article three will be served here');   
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
