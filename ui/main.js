23d500f68f2ec1512f25ed3d60c5842ff3fd51fb
sruthilohi <sruthilohi@gmail.com>
2016-10-07 10:29:57 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 // change the text of the main text div
-var element = document.getelementbyid('main text'
-);
-element.inerthtml='New Value';
+//var element = document.getelementbyid('main text'
+//);
+//element.inerthtml='New Value';
 var img=document.getelementbyid('madi');
 img.onclick= function(){
  img.style.marginleft='100px';   
