4a39845bff4fa0fe0d69c0d81dd943623d803d42
sruthilohi <sruthilohi@gmail.com>
2016-10-20 17:48:10 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,9 @@
 console.log('Loaded!');
-/* change the text of the main text div
+ //change the text of the main text div
 var element = document.getElementById('main-text'
 );
-element.innerhtml='new value'
-var img=document.getElementById('madi');
-img.onclick= function(){
- img.style.marginleft='100px';   
-};*/
+element.innerhtml='new value';
+//var img=document.getElementById('madi');
+/*img.onclick= function(){
+ img.style.marginleft='100px'; */  
+//};
