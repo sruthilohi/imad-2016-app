ffb94b856a324ab991ca01c1c32bc30099674c30
sruthilohi <sruthilohi@gmail.com>
2016-10-21 11:57:32 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,19 +8,22 @@ img.onclick= function(){
  img.style.marginleft='100px';  
 }; */
 //counter code
-//var counter=document.getElementById('counter');
-//button.onclick=function(){
+
+var counter=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
     
     
-    //make a request to the counter end point
+   // make a request to the counter end point
     
-    //capture the response and store it in a variable
+   // capture the response and store it in a variable
     
     // render the variable in the correct span
+    counter=counter+1;
     
-//   var span= document.getElementById('count');
-//span.innerhtml=counter.tostring();
-
+   var span= document.getElementById('count');
+//span.innerhtmlcounter.toString();
+span.innerHTML=(counter.toString());
     
-//};
-//console.log(counter.toString());
+};
+//span.innerHTML=(counter.toString());
