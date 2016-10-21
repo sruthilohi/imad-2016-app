f8dabe792c16085aebb0404394e0d23c89324b57
sruthilohi <sruthilohi@gmail.com>
2016-10-21 10:06:16 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,8 +2,24 @@ console.log('Loaded!');
  //change the text of the main text div
 //var element = document.getElementById('main-text'
 //);
-//element.innerhtml='new value';
+/* element.innerhtml='new value';
 var img=document.getElementById('madi');
 img.onclick= function(){
  img.style.marginleft='100px';  
+}; */
+//counter code
+var counter=document.getElementById('counter');
+button.onclick=function(){
+    
+    
+    //make a request to the counter end point
+    
+    //capture the response and store it in a variable
+    
+    // render the variable in the correct span
+    
+    counter =counter+1;
+   var span= document.getElementById('count');
+   
+    
 };
