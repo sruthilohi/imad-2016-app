2efc650a1469153ece9b512578eb1e8a71a74f00
sruthilohi <sruthilohi@gmail.com>
2016-10-21 08:20:08 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,10 +110,10 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var c=0;
-app.get('/counter' , function(req,res){
-    c=c+1;
-    res.send(c.tostring());
+var counter=0;
+app.get('/counter', function(req,res){
+    counter=counter+1;
+    res.send(counter.tostring());
 });
 
 app.get('/:articlename', function(req,res){
