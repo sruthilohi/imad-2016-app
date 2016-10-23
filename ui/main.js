2b60d95c7723000001329431a294ed016384127c
sruthilohi <sruthilohi@gmail.com>
2016-10-23 08:13:20 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,7 @@ button.onclick = function(){
             //take some action
             if(request.status === 200){
                  var counter = request.responseText;
+                 counter=counter+1;
                  var span= document.getElementById('count');
 
                  span.innerHTML=counter.toString();
