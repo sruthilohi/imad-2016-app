a979346752d83c3bd8ae38938b0370ff84f84d1a
sruthilohi <sruthilohi@gmail.com>
2016-10-30 10:43:47 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,16 +145,16 @@ app.get('/articles/:articlename', function(req,res){
     
     //articlename==article-one
     //articles[articlename]=={} content object for article-one
-  //var articlename=req.params.articlename;
-  pool.query=("select *from article where title= '" +req.params.articlename "'", function(err,result){
+ // var articlename = req.params.articlename;
+  pool.query=("select * from article where title = " +req.params.articlename , function(err,result){
       if(err) {
           res.status(500).send(err.toString());
           
       } else {
-          if(result.rows.length===0){
+          if(result.rows.length === 0){
               res.status(404).send('article not found');
            }else {
-               var articleData= result.rows[0];
+               var articleData = result.rows[0];
                 res.send(createtemplate(articleData));
            }
       }
