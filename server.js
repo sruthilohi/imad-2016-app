ab573d143c73646955b14aba06cec8671d44f72c
sruthilohi <sruthilohi@gmail.com>
2016-11-12 15:42:29 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -192,8 +192,9 @@ app.get('/ui/madi.png', function (req, res) {
 
 });*/
 var names = [];
-app.get('/submit-name/:name', function(req,res){
-    var name = req.params.name;
+app.get('/submit-name', function(req,res){  //submit-name?name=xxx
+// get the name from the request
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
     
