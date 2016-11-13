b80b58718d090979b178bdb355b89729456c9abb
sruthilohi <sruthilohi@gmail.com>
2016-11-13 17:34:15 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -81,8 +81,9 @@ submit.onclick = function(){
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
+    
    request.open('POST','http://sruthilohi.imad.hasura-app.io/login', true);
-    request.setRequestHeader('content-type', 'application/json');
+    request.setRequestHeader('Content-Type', 'application/json');
       request.send(JSON.stringify({username: username, password:password}));
     
     
