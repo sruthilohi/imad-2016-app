7b711a08980b8a70fdf547d8d919d60453276b05
sruthilohi <sruthilohi@gmail.com>
2016-11-18 08:16:29 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,6 +57,46 @@ function loadLoginForm(){
        `;
      document.getElementById('login_area').innerHTML = loginHtml;  
    
+   /*  var submit = document.getElementById('login_btn');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };  */
+   
+   
+   
+   
    
      //submit username and password to login
 var submit = document.getElementById('login_btn');
@@ -90,17 +130,17 @@ submit.onclick = function(){
         //not done yet
  };
     //make a request
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    console.log(username);
-    console.log(password);
-    
-   //request.open('POST','http://sruthilohi.imad.hasura-app.io/login', true);
-    request.open('POST','/login', true);
-    request.setRequestHeader('Content-Type', 'application/json');
-     // request.send(JSON.stringify({username: username, password: password}));
-     request.send(JSON.stringify({username: username, password: password}));  
-    //submit.value = 'Logging in...';
+   
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+   
     // make a request to the server and send the name
     
     
