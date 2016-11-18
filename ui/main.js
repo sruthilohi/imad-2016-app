74168019c291f32696e7982293a3adeccf3d430a
sruthilohi <sruthilohi@gmail.com>
2016-11-18 19:06:19 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -129,28 +129,53 @@ function loadLoginForm(){
     
     };
 }
-
+/* function loadArticles () {
+        // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
+            if (request.status === 200) {
+                var content = '<ul>';
+                var articleData = JSON.parse(this.responseText);
+                for (var i=0; i< articleData.length; i++) {
+                    content += `<li>
+                    <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
+                    (${articleData[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
+            } else {
+                articles.innerHTML('Oops! Could not load all articles!')
+            }
+        }
+    };
+    
+    request.open('GET', '/get-articles', true);
+    request.send(null);
+}
+ */
 
 
 function loadarticles () {
     // Check if the user is already logged in
     var request = new XMLHttpRequest();
     request.onreadystatechange = function () {
-        var articles = document.getElementById('articles');
+        
         
         if (request.readyState === XMLHttpRequest.DONE) {
+            var articles = document.getElementById('articles');
             if (request.status === 200) {
                 var content = '<ul>';
                 
                var articledata = JSON.parse(this.responseText);
                for (var i=0; i< articledata.length ; i++){
-                   content +=  `<li> 
-                   <a href="/articles/{articledata[i].title}">${articleData[i].heading}</a>
-                   (${articleData[i].date.split('T')[0]})</li>`;
-                   
-               }
-               content+="</ul>"
-               articles.innerHTML = content;
+                   content += `<li>
+                    <a href="/articles/${articledata[i].title}">${articledata[i].heading}</a>
+                    (${articledata[i].date.split('T')[0]})</li>`;
+                }
+                content += "</ul>"
+                articles.innerHTML = content;
             } else {
               articles.innerHTML = ('Ooops! could not load all articles');  
             }
@@ -178,3 +203,4 @@ function loadLogin () {
     request.send(null);
 }
 loadLoginForm ();
+loadarticles();
