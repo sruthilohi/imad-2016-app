16150030048871a09383a6e32a3703f81d67f8d8
sruthilohi <sruthilohi@gmail.com>
2016-11-18 10:48:20 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -57,13 +57,16 @@ function loadLoginForm(){
        `;
      document.getElementById('login_area').innerHTML = loginHtml;  
    
-   /*  var submit = document.getElementById('login_btn');
+   
+   
+     // Submit username/password to login
+    var submit = document.getElementById('login_btn');
     submit.onclick = function () {
         // Create a request object
-        var request = new XMLHttpRequest();
+         var request = new XMLHttpRequest();
         
         // Capture the response and store it in a variable
-        request.onreadystatechange = function () {
+                request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
               // Take some action
               if (request.status === 200) {
@@ -92,59 +95,7 @@ function loadLoginForm(){
         request.send(JSON.stringify({username: username, password: password}));  
         submit.value = 'Logging in...';
         
-    };  */
-   
-   
-   
-   
-   
-     //submit username and password to login
-var submit = document.getElementById('login_btn');
-
-submit.onclick = function(){
-    
-   //create a request object
-   
-   var request = new XMLHttpRequest();
-    
-   // capture the response and store it in a variable
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE){
-            //take some action
-            if (request.status === 200){
-                console.log('user logged in');
-                alert('logged in successfully');
-               
-                } else if (request.status===403){
-                    alert('username/password is incorrect');
-                   
-                } else if (request.status===500){
-                    alert('something went wrong on the server');
-                  
-                }
-               
-               
-          
-            
-       }
-        //not done yet
- };
-    //make a request
-   
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
-        
-        request.open('POST', 'https://sruthilohi.imad.hasura-app.io/login', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
-        submit.value = 'Logging in...';
-   
-    // make a request to the server and send the name
-    
-    
-};
+    };
  var register = document.getElementById('register_btn');
     register.onclick = function () {
         // Create a request object
@@ -176,5 +127,22 @@ submit.onclick = function(){
         register.value = 'Registering...';
     
     };
+}
+
+function loadLogin () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
 }
 loadLoginForm ();
