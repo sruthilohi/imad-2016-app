3af39dd792ebd73773a83ebd2d540506f44d2e47
sruthilohi <sruthilohi@gmail.com>
2016-11-18 10:20:37 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -167,10 +167,11 @@ submit.onclick = function(){
         // Make the request
         var username = document.getElementById('username').value;
         var password = document.getElementById('password').value;
-        console.log(username);
-        console.log(password);
+        
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
+        console.log(username);
+        console.log(password);
         request.send(JSON.stringify({username: username, password: password}));  
         register.value = 'Registering...';
     
