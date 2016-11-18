73bcc28af9f8d97631246d51a0b58c0fc0c50325
sruthilohi <sruthilohi@gmail.com>
2016-11-18 18:06:17 +0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -104,7 +104,8 @@ function loadLoginForm(){
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
-              // Take some action
+              
+             
               if (request.status === 200) {
                   alert('User created successfully');
                   register.value = 'Registered!';
@@ -127,6 +128,23 @@ function loadLoginForm(){
         register.value = 'Registering...';
     
     };
+}
+function loadarticles () {
+    // Check if the user is already logged in
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
+            }
+        }
+    };
+    
+    request.open('GET', '/check-login', true);
+    request.send(null);
 }
 
 function loadLogin () {
