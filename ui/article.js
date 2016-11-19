72326504797cf9d8a397cb4887b7ab7144a5320e
sruthilohi <sruthilohi@gmail.com>
2016-11-19 12:58:50 +0400

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,15 +1,46 @@
-console.log('article.js');
+
 function loadCommentForm () {
     var CommentFormHtml = `
         <h5>Submit a comment</h5>
         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comments here..."></textarea>
        
         <br/>
-       <input type="submit" id="register_btn" value="Register" />
+       <input type="submit" id="submit" value="submit" />
         <br/>
         `;
         
     document.getElementById('comment_form').innerHTML =  CommentFormHtml;
     
+    var submit = document.getElementById('submit');
+    
+    submit.onclick = function () {
+        // Create a request object
+         var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+                request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  document.getElementById('comment_text').value = '';
+                    loadComments();    
+              } else {
+                 alert('Error! Could not submit comment');
+              } 
+              submit.value = 'Submit';
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var commet = document.getElementById('comment_text').value;
+        
+        console.log(comment);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    }; 
     
 }
