e550d82b6c00514963f57f5c7b967461263f1e54
sruthilohi <sruthilohi@gmail.com>
2016-11-19 11:01:31 +0400

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -1,1 +1,15 @@
 console.log('article.js');
+function loadCommentForm () {
+    var CommentFormHtml = `
+        <h5>Submit a comment</h5>
+        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comments here..."></textarea>
+       
+        <br/>
+       <input type="submit" id="register_btn" value="Register" />
+        <br/>
+        `;
+        
+    document.getElementById('comment_form').innerHTML =  CommentFormHtml;
+    
+    
+}
