f455069d9dc8def69d7e406a5594c85a246d4678
sruthilohi <sruthilohi@gmail.com>
2016-11-19 23:07:28 +0400

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -48,9 +48,8 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                loadLoggedInUser(this.responseText);
-            } else {
-                loadLoginForm();
+                loadCommentForm(this.responseText);
+            
             }
         }
     };
