ede22c86df27d8925781a114f9f87084cf232f98
sruthilohi <sruthilohi@gmail.com>
2016-11-25 17:47:38 +0400

    [imad-console] Updates ui/newarticle.js

--- ui/newarticle.js
+++ ui/newarticle.js
@@ -1,1 +1,15 @@
-console.log('Loaded');
+function loadArticleForm () {
+    var articleFormHtml = `
+        <div>
+                  <a href="/">Home</a>
+        </div>
+        <h5>Submit an Article </h5>
+         <input type = "text" id="title" placeholder="Title"/>
+          <input type = "text" id="heading" placeholder="Heading"/>
+        <textarea id="content_text" rows="5" cols="100" placeholder="Enter your article here..."></textarea>
+        <br/>
+        <input type="submit" id="submit" value="Submit" />
+        <br/>
+        `;
+    document.getElementById('comment_form').innerHTML = commentFormHtml;
+}
