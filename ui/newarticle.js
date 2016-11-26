42d0fda10bd163ab4f2238e3a03b3b1382ab84eb
sruthilohi <sruthilohi@gmail.com>
2016-11-26 08:48:04 +0400

    [imad-console] Updates ui/newarticle.js

--- ui/newarticle.js
+++ ui/newarticle.js
@@ -5,11 +5,42 @@ function loadArticleForm () {
         </div>
         <h5>Submit an Article </h5>
          <input type = "text" id="title" placeholder="Title"/>
-          <input type = "text" id="heading" placeholder="Heading"/>
+          <input type = "text" id=heading" placeholder="Heading"/>
         <textarea id="content_text" rows="5" cols="100" placeholder="Enter your article here..."></textarea>
         <br/>
-        <input type="submit" id="submit" value="Submit" />
+        <input type="submit" id="ArticleSubmit" value="Submit" />
         <br/>
         `;
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    document.getElementById('new_article').innerHTML = articleFormHtml;
+    
+  
+    var submit = document.getElementById('ArticleSubmit');
+    submit.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+                // Take some action
+                if (request.status === 200) {
+                    
+                } else {
+                    alert('Error! Could not submit Article');
+                }
+                submit.value = 'Submit';
+          }
+        };
+        
+        // Make the request
+        var title = document.getElementById('title').value;
+         var heading = document.getElementById('heading').value;
+          var content = document.getElementById('content_text').value;
+        request.open('POST', '/submit-article', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({title:title, heading:heading, content:content  }));  
+        submit.value = 'Submitting...';
+        
+    };
 }
+loadArticleForm();
