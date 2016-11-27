1a8287ccd0b98b598be3442a2c9d509259a8a98e
sruthilohi <sruthilohi@gmail.com>
2016-11-27 10:18:34 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -285,11 +285,11 @@ app.post('/submit-comment/:articleName', function (req, res) {
     } 
         
     });
-});
+});*/
 
  app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});*/
+});
 
 /*var counter=0;
 app.get('/counter', function(req,res){
