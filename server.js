240bcda979756902774fe0498850b0c17a97b559
sruthilohi <sruthilohi@gmail.com>
2016-12-04 08:20:39 +0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,25 +130,7 @@ app.get('/hash/:input', function(req,res){
    res.send(hashedstring);
 });
 
-/*
-app.post('/create-user', function (req, res) {
-      var username = req.body.username;
-      var password = req.body.password;
-      if(!username.trim() || !password.trim()){
-           res.status(400).send('Username or password field blank.');   //Err if blank,tabs and space detected.
-       } else{
-            var salt = crypto.randomBytes(128).toString('hex');
-            var dbString = hash(password, salt);
-            pool.query('INSERT INTO "user" (username, password) VALUES ($1, $2)', [username, dbString], function (err, result) {
-        if(err) {
-           res.status(500).send(err.toString());
-        } else {
-           res.send('User successfully created: ' + username);
-        }
-      });
-     }
-});
-*/
+
 
 app.post('/create-user', function(req,res){
     var username = req.body.username;
